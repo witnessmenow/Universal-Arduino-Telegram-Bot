ee63874bcec508003b3243528eff026c1563e480
Malte <SkyGuardian42@gmail.com>
2017-02-06 19:16:36 +0100

    Removed double entry in menu

--- examples/ESP8266/CustomKeyboard/ReplyKeyboardMarkup/ReplyKeyboardMarkup.ino
+++ examples/ESP8266/CustomKeyboard/ReplyKeyboardMarkup/ReplyKeyboardMarkup.ino
@@ -64,7 +64,6 @@ void handleNewMessages(int numNewMessages) {
       String welcome = "Welcome to Universal Arduino Telegram Bot library, " + from_name + ".\n";
       welcome += "This is Reply Keyboard Markup example.\n\n";
       welcome += "/ledon : to switch the Led ON\n";
-      welcome += "/ledon : to switch the Led ON\n";
       welcome += "/ledoff : to switch the Led OFF\n";
       welcome += "/status : Returns current status of LED\n";
       welcome += "/options : returns the reply keyboard\n";
