9bab9722c84c0d0cf38464140ed496975581a0f1
witnessmenow <ecenuig@gmail.com>
2020-06-07 19:37:08 +0100

    commenting out debug logging by deafult

--- src/UniversalTelegramBot.h
+++ src/UniversalTelegramBot.h
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 #ifndef UniversalTelegramBot_h
 #define UniversalTelegramBot_h
 
-#define TELEGRAM_DEBUG 1
+//#define TELEGRAM_DEBUG 1
 #define ARDUINOJSON_DECODE_UNICODE 1
 #define ARDUINOJSON_USE_LONG_LONG 1
 #include <Arduino.h>
