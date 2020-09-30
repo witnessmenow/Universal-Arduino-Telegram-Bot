92fa5f473710f08b5404982bf170680a9bf97c28
Bolukan <github@bolukan.nl>
2020-09-30 22:06:46 +0200

    Update 101 examples

--- examples/Untested/101/FlashledBot/FlashledBot.ino
+++ examples/Untested/101/FlashledBot/FlashledBot.ino
@@ -6,56 +6,61 @@
  *  adapted by Brian Lough                                         *
  *******************************************************************/
 
- #include <WiFiSSLClient.h>
- #include <WiFi101.h>
- #include <UniversalTelegramBot.h>
+#include <WiFiSSLClient.h>
+#include <WiFi101.h>
+#include <UniversalTelegramBot.h>
+#include <SPI.h>
 
+// Wifi network station credentials
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASSWORD "YOUR_PASSWORD"
+// Telegram BOT Token (Get from Botfather)
+#define BOT_TOKEN "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
-// Initialize Wifi connection to the router
-char ssid[] = "xxxxxxxxxxxxxxxxxxxxxx";              // your network SSID (name)
-char password[] = "yyyyyyyy";                              // your network key
-
-
-
-const int ledPin = 13;
-
-// Initialize Telegram BOT
-#define BOTtoken "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  // your Bot Token (Get from Botfather)
+const unsigned long BOT_MTBS = 1000; // mean time between scan messages
 
 WiFiSSLClient client;
-UniversalTelegramBot bot(BOTtoken, client);
+UniversalTelegramBot bot(BOT_TOKEN, client);
+unsigned long bot_lasttime; // last time messages' scan has been done
 
-int Bot_mtbs = 1000; //mean time between scan messages
-long Bot_lasttime;   //last time messages' scan has been done
+const int ledPin = 13;
 bool Start = false;
 int ledStatus = 0;
 
-void handleNewMessages(int numNewMessages) {
-  Serial.println("handleNewMessages");
-  Serial.println(String(numNewMessages));
-  for(int i=0; i<numNewMessages; i++) {
-    String chat_id = String(bot.messages[i].chat_id);
+void handleNewMessages(int numNewMessages)
+{
+  for (int i = 0; i < numNewMessages; i++)
+  {
+    String chat_id = bot.messages[i].chat_id;
     String text = bot.messages[i].text;
-    if (text == "/ledon") {
-      digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+    if (text == "/ledon")
+    {
+      digitalWrite(ledPin, HIGH); // turn the LED on (HIGH is the voltage level)
       ledStatus = 1;
       bot.sendMessage(chat_id, "Led is ON", "");
     }
-    if (text == "/ledoff") {
+    if (text == "/ledoff")
+    {
       ledStatus = 0;
-      digitalWrite(ledPin, LOW);    // turn the LED off (LOW is the voltage level)
+      digitalWrite(ledPin, LOW); // turn the LED off (LOW is the voltage level)
       bot.sendMessage(chat_id, "Led is OFF", "");
     }
-    if (text == "/status") {
-      if(ledStatus){
+    if (text == "/status")
+    {
+      if (ledStatus)
+      {
         bot.sendMessage(chat_id, "Led is ON", "");
-      } else {
+      }
+      else
+      {
         bot.sendMessage(chat_id, "Led is OFF", "");
       }
     }
-    if (text == "/start") {
+    if (text == "/start")
+    {
       String from_name = bot.messages[i].from_name;
-      if (from_name == "") from_name = "Anonymous";
+      if (from_name == "")
+        from_name = "Anonymous";
 
       String welcome = "Welcome, " + from_name + ", from FlashLedBot, your personal Bot on Arduino 101\n";
       welcome = welcome + "/ledon : to switch the Led ON \n";
@@ -66,40 +71,40 @@ void handleNewMessages(int numNewMessages) {
   }
 }
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-
-  delay(100);
+  Serial.println();
 
   // attempt to connect to Wifi network:
-  Serial.print("Connecting Wifi: ");
-  Serial.println(ssid);
-  while (WiFi.begin(ssid, password) != WL_CONNECTED) {
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(WIFI_SSID);
+  while (WiFi.begin(WIFI_SSID, WIFI_PASSWORD) != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
 
   pinMode(ledPin, OUTPUT); // initialize digital ledPin as an output.
   delay(10);
   digitalWrite(ledPin, LOW); //initialize pin as off
-
 }
 
-void loop() {
-
-  if (millis() > Bot_lasttime + Bot_mtbs)  {
+void loop()
+{
+  if (millis() - bot_lasttime > BOT_MTBS)
+  {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    while(numNewMessages) {
+
+    while (numNewMessages)
+    {
       Serial.println("got response");
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
-    Bot_lasttime = millis();
+
+    bot_lasttime = millis();
   }
 }
